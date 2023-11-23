69d665ec6aeb04dd74763a5307278f700f07112e
Adaobilynda1234 <adaobilynda1234@gmail.com>
2023-11-23 14:09:32 +0100

    fourth commit

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+import { useLiveQuery } from "dexie-react-hooks";
 import getPhotoUrl from "get-photo-url";
+import { db } from "../dexie";
 
 const Gallery = () => {
-  const [allPhotos, setallPhotos] = useState([]);
+  const allPhotos = useLiveQuery(() => db.gallery.toArray(), []);
 
   const addPhoto = async () => {
-    const newPhoto = {
-      id: Date.now(),
+    db.gallery.add({
       url: await getPhotoUrl("#addPhotoInput"),
-    };
-    setallPhotos([newPhoto, ...allPhotos]);
+    });
   };
+
+  const removePhoto = (id) => {
+    db.gallery.delete(id);
+  };
+
   return (
-    <div>
-      <input type="file" name="photo" id="addPhotoInput" />
+    <>
+      <input type="file" accept="image/*" name="photo" id="addPhotoInput" />
       <label htmlFor="addPhotoInput" onClick={addPhoto}>
-        <i className="add-photo-button fas fa-plus-square"></i>
+        <i className="add-photo-button fas fa-plus-square" />
       </label>
+
       <section className="gallery">
-        {allPhotos.map((photo) => (
+        {!allPhotos && <p>Loading...</p>}
+        {allPhotos?.map((photo) => (
           <div className="item" key={photo.id}>
             <img src={photo.url} className="item-image" alt="" />
-            <button className="delete-button">Delete</button>
+            <button
+              className="delete-button"
+              onClick={() => removePhoto(photo.id)}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </section>
-    </div>
+    </>
   );
 };
 
